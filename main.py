4e352fa00d7dd114084874fe22e634048a7bc9eb
KatyMartin23 <832006@mystma.org>
2021-02-23 16:15:05 +0000

    Evens 3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def removeEvens(k):
     NewList = k
     numPop = 0
     for i in range (0, len(k)):
-        if NewList[i - numPop] % 2 == 0 and newList[i - numPop] != 0:
+        if NewList[i - numPop] % 2 == 0 and NewList[i - numPop] != 0:
             NewList.pop(i - numPop)
             numPop = numPop + 1
     return NewList
